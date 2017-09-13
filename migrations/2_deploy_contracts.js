257f959cb2104045ba3eba32b702ea6ab5fe5a9b
Jaynti Kanani <jdkanani@gmail.com>
2017-09-13 23:28:16 +0530

    deployer

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,6 +1,33 @@
-var MultiSigWalletFactory = artifacts.require("./wallet/MultiSigWalletFactory.sol");
+let Migrations = artifacts.require("./Migrations.sol");
+let RegistryFactory = artifacts.require("./registry/RegistryFactory.sol");
+let Registry = artifacts.require("./registry/Registry.sol");
+let MultiSigWalletFactory = artifacts.require("./wallet/MultiSigWalletFactory.sol");
+let MultiSigWallet = artifacts.require("./wallet/MultiSigWallet.sol");
 
-module.exports = function(deployer) {
+module.exports = async function(deployer) {
+  // Deploy migration contract
+  // await deployer.deploy(Migrations);
+
+  //
+  // Get registry from registry factory
+  //
+
+  // Deploy registry factory
+  await deployer.deploy(RegistryFactory);
+  const registryFactory = await RegistryFactory.deployed();
+
+  // Create registry contract
+  const registryReceipt = await registryFactory.create();
+  const registryContract = Registry.at(registryReceipt.logs[0].args._address);
+
   // Deploy multisig wallet factory
-  deployer.deploy(MultiSigWalletFactory);
+  await deployer.deploy(MultiSigWalletFactory);
+  const multisigWalletFactory = await MultiSigWalletFactory.deployed();
+
+  // Logs
+  console.log("============================================================");
+  console.log("Registry factory address: ", registryFactory.address);
+  console.log("Registry address: ", registryContract.address);
+  console.log("Multisig wallet factory address: ", multisigWalletFactory.address);
+  console.log("============================================================");
 };

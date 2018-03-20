f2f8311edf7db2004de20e31af5b2e65cef185f0
Jaynti Kanani <jdkanani@gmail.com>
2018-03-20 19:30:38 +0530

    migrate script

--- truffle.js
+++ truffle.js
@@ -8,5 +8,10 @@ module.exports = {
   networks: {
     // See <http://truffleframework.com/docs/advanced/configuration>
     // to customize your Truffle configuration!
+    dev: {
+      host: '127.0.0.1',
+      port: 8545,
+      network_id: 4474 // Match any network id
+    }
   }
 }
